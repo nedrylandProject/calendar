d4872ed404dddee9086e3fe5fdda8fb848857c84
DESKTOP-0B0SRN2\John <source@mitedu.eu>
2024-08-01 12:07:08 +0200

    Another commit

--- save_day_color.php
+++ save_day_color.php
@@ -3,6 +3,70 @@
 include('db.php');
 
 
+function sendSMS($phon,$smsmessage)
+{
+
+        $budgetsms="578f5648d7db40320fb963dda5ecb05b";
+        //$user = $_SESSION["jpos"];
+        $phon = "00421908888866";
+        
+        
+		$api_url = 'https://api.budgetsms.net/sendsms/';
+		
+		if($smsmessage!='' && $phon!='') {
+
+
+		// API key and other parameters
+		$username = 'zemkcomsms';
+		$userid = '18842';
+		$handle = $budgetsms; // handle set in credentials file
+		//$phone_number = '00421908888866';
+		$phone_number = $phon;
+		//$from = '00421951023045';
+		$from = 'CALI';
+
+		// Create an array with the data you want to send
+		$data = array(
+			'username' => $username,
+			'userid' => $userid,
+			'handle' => $handle,
+			'to' => $phone_number,
+			'msg' => $smsmessage,
+			'from' => $from
+		);
+
+		// Initialize cURL session
+		$ch = curl_init();
+
+		// Set cURL options
+		curl_setopt($ch, CURLOPT_URL, $api_url);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		// Execute the cURL session and store the result in $response
+		$response = curl_exec($ch);
+
+		// Check for cURL errors
+		if (curl_errno($ch)) {
+			echo 'cURL error: ' . curl_error($ch);
+		}
+
+		// Close the cURL session
+		curl_close($ch);
+
+		// Output the API response
+		return 'API Response: ' . $response . '\n';
+		
+
+		} else 
+		    { 
+		           return "API Response: No USER, Number or Oncall./n";
+		        
+		    }
+}
+
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -43,6 +107,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 echo "Error: " . $sql . "<br>" . $conn->error;
                 exit;
             }
+			
+			
         }
 
         // Insert or update the user_class in event_users table
@@ -52,6 +118,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($conn->query($sql) === TRUE) {
         echo "Record updated successfully";
+		
+		sendSMS("number","User " . $userClass . " just made update on ".$day.".".$month.".");
+		
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
@@ -73,5 +142,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 $conn->close();
-?>
 
+
+
+
+
+?>
